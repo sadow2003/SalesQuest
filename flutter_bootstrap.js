bd29ff46fa2a9bf7954171023f254cecfd7b70cc
Zougagh_Mounsif <zougaghmounsif.work@gmail.com>
2026-06-26 10:54:06 +0100

    Fix base-href to /SalesQuest/ so assets load from correct path

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2230987244" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2396964167" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
